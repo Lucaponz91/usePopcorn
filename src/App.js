3759772f0b64cfef211965ea56ab77c68a013211
Lucaponz91 <lucapo1991@msn.com>
2023-09-14 00:19:34 +0200

    add star component

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
+        
         <Box>
           <MovieList movies={movies} />
         </Box>
